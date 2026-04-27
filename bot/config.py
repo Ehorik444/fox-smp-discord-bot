9b5ff9b0f028cc2a6a63e8584666c5296bc01416
Ehorik444 <155249424+Ehorik444@users.noreply.github.com>
2026-04-27 05:29:47 +0300

    Add validation for GUILD_ID, VOICE_CATEGORY_ID, and TICKET_CATEGORY_ID
    Added checks for environment variables to ensure they are set and converted to integers.

--- bot/config.py
+++ bot/config.py
@@ -4,7 +4,19 @@ from dotenv import load_dotenv
 load_dotenv()
 
 TOKEN = os.getenv("TOKEN")
-GUILD_ID = int(os.getenv("GUILD_ID"))
 
-VOICE_CATEGORY_ID = int(os.getenv("VOICE_CATEGORY_ID"))
-TICKET_CATEGORY_ID = int(os.getenv("TICKET_CATEGORY_ID"))
+GUILD_ID = os.getenv("GUILD_ID")
+VOICE_CATEGORY_ID = os.getenv("VOICE_CATEGORY_ID")
+TICKET_CATEGORY_ID = os.getenv("TICKET_CATEGORY_ID")
+
+# защита от None
+if GUILD_ID:
+    GUILD_ID = int(GUILD_ID)
+else:
+    raise ValueError("GUILD_ID не задан в .env")
+
+if VOICE_CATEGORY_ID:
+    VOICE_CATEGORY_ID = int(VOICE_CATEGORY_ID)
+
+if TICKET_CATEGORY_ID:
+    TICKET_CATEGORY_ID = int(TICKET_CATEGORY_ID)
