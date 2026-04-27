975fb4a403c08faefba1b4bb2180dd6988d7d3fc
Ehorik444 <155249424+Ehorik444@users.noreply.github.com>
2026-04-27 05:11:57 +0300

    Add get_server_status function for Minecraft bot

---
+++ bot/minecraft.py
@@ -0,0 +1,7 @@
+def get_server_status():
+    # сюда позже можно подключить RCON / API / plugin
+    return {
+        "online": True,
+        "players": ["Steve", "Alex"],
+        "max": 20
+    }
