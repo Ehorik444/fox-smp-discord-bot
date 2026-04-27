91aa25127400a92599506400cb1432aa89966f14
Ehorik444 <155249424+Ehorik444@users.noreply.github.com>
2026-04-27 06:15:14 +0300

    Add create_report function to manage reports

--- bot/systems/report.py
+++ bot/systems/report.py
@@ -1,1 +1,12 @@
+reports = []
 
+
+def create_report(reporter, target, reason):
+    report = {
+        "reporter": reporter,
+        "target": target,
+        "reason": reason
+    }
+
+    reports.append(report)
+    return report
