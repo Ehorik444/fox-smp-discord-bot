47baceaed9c7095f46fdf52d10caf3ba31f0a352
Ehorik444 <155249424+Ehorik444@users.noreply.github.com>
2026-04-27 22:01:54 +0300

    Update rcon.py

--- bot/rcon.py
+++ bot/rcon.py
@@ -1,13 +1,15 @@
 from mcrcon import MCRcon
+import os
 
-RCON_HOST = "localhost"
-RCON_PORT = 25575
-RCON_PASSWORD = "YOUR_PASSWORD"
+RCON_HOST = os.getenv("RCON_HOST")
+RCON_PORT = int(os.getenv("RCON_PORT", "25575"))
+RCON_PASSWORD = os.getenv("RCON_PASSWORD")
+
 
 def run_rcon(command: str):
     try:
-        with MCRcon(RCON_HOST, RCON_PASSWORD, RCON_PORT) as mcr:
+        with MCRcon(RCON_HOST, RCON_PASSWORD, port=RCON_PORT) as mcr:
             return mcr.command(command)
     except Exception as e:
-        print("[RCON ERROR]", e)
-        return ""
+        print("RCON error:", e)
+        return None
