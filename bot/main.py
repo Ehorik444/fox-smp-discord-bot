8629836bdb1f0b6f1ec79114ca370f6009d053f3
Ehorik444 <155249424+Ehorik444@users.noreply.github.com>
2026-04-29 16:55:08 +0300

    Enhance bot setup with database and member intents
    Updated bot initialization to include database connection and improved member intents.

--- bot/main.py
+++ bot/main.py
@@ -1,31 +1,34 @@
 import discord
 from discord.ext import commands
-import asyncio
 import os
+from dotenv import load_dotenv
+from db import db
+
+load_dotenv()
 
 TOKEN = os.getenv("TOKEN")
 
+
 intents = discord.Intents.default()
-intents.message_content = True
+intents.members = True
 
 bot = commands.Bot(command_prefix="!", intents=intents)
 
-EXTENSIONS = ["applications"]
 
 async def load_extensions():
-    for ext in EXTENSIONS:
-        try:
-            await bot.load_extension(ext)
-            print(f"✅ Loaded {ext}")
-        except Exception as e:
-            print(f"❌ Error loading {ext}: {e}")
+    await bot.load_extension("applications")
+
 
 @bot.event
 async def setup_hook():
+    await db.connect()
+    await db.init()
     await load_extensions()
+
 
 @bot.event
 async def on_ready():
-    print(f"Bot online as {bot.user}")
+    print(f"Logged in as {bot.user}")
+
 
 bot.run(TOKEN)
